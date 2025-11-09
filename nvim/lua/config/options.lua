fbb27f8663a3e924f46daa4b71f6161d0cea36a2
0xh4kv <h4kvx0@hotmail.com>
2025-11-09 16:51:00 -0300

    feat: add neovim config with lazyvim

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,25 @@
+--  ________          __  .__
+--  \_____  \ _______/  |_|__| ____   ____   ______
+--   /   |   \\____ \   __\  |/  _ \ /    \ /  ___/
+--  /    |    \  |_> >  | |  (  <_> )   |  \\___ \
+--  \_______  /   __/|__| |__|\____/|___|  /____  >
+--          \/|__|                       \/     \/
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 8
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.mouse = ""
